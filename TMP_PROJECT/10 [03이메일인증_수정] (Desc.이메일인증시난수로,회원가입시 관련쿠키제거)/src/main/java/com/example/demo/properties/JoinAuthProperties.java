a8596f72a68405624f559c8552a3e57ff4d29fb0
jungwoogyun <jwg8910@naver.com>
2024-03-18 17:49:59 +0900

    .

--- TMP_PROJECT/10 [03이메일인증_수정] (Desc.이메일인증시난수로,회원가입시 관련쿠키제거)/src/main/java/com/example/demo/properties/JoinAuthProperties.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.properties;
-
-public class JoinAuthProperties {
-
-    public final static String COOKIE_NAME="Join_Auth_";
-    public final static String TOKEN_ID="Join_Auth_";
-}
