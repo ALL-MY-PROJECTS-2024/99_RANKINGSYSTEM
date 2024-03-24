6caabda8023d693f56d87e7b013caa4a7c68776c
jungwoogyun <jwg8910@naver.com>
2024-03-24 13:32:22 +0900

    .

--- src/main/java/com/creator/imageAndMusic/properties/UPLOADPATH.java
+++ src/main/java/com/creator/imageAndMusic/properties/UPLOADPATH.java
@@ -1,13 +1,11 @@
 package com.creator.imageAndMusic.properties;
 
-import java.io.File;
-
 public class UPLOADPATH {
 
     //WINDOW
-    public final static String ROOTDIRPATH = "c:" + File.separator;
+    //public final static String ROOTDIRPATH = "c:" + File.separator;
 
-    //public final static String ROOTDIRPATH = "/";
+    public final static String ROOTDIRPATH = "/";
 
     public final static String UPPERDIRPATH = "upload";
     public final static String IMAGEDIRPATH ="images";
