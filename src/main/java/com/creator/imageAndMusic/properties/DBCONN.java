bb1fec5a94d23b8e1624c1b7851da01e964dafbf
gloryofalive <gloryofalive@naver.com>
2024-03-24 15:43:40 +0900

    .

--- src/main/java/com/creator/imageAndMusic/properties/DBCONN.java
+++ src/main/java/com/creator/imageAndMusic/properties/DBCONN.java
@@ -2,7 +2,7 @@ package com.creator.imageAndMusic.properties;
 
 public class DBCONN {
 
-    public static final String URL = "jdbc:mysql://13.125.168.113:3306/CreatorDB";
+    public static final String URL = "jdbc:mysql://localhost:3306/CreatorDB";
     public static final String ID = "dbconn";
     public static final String PW = "Zhfldk11!";
 
