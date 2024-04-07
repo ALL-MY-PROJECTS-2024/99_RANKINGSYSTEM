378cf86a29ad03b079bc6d9e09d19d35ecad439a
gloryofalive <gloryofalive@naver.com>
2024-04-07 17:03:53 +0900

    .

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,10 +1,15 @@
+const swiper_vertical = new Swiper('.top-header .left .swiper', {
+direction: 'vertical',
+        autoplay:{
+            delay:3000,
+        },
+        loop: true,
+});
+
 
 let rightText = document.querySelector(".right-2");
-
 window.addEventListener("scroll", function () {
     let value = window.scrollY;
-
-
 
     if(value>400){
         console.log(value);
@@ -12,4 +17,7 @@ window.addEventListener("scroll", function () {
         rightText.style.opacity = "1";
     }
 });
+
+
+
 
