6b2ec0ea28201e4b5e0fd392e9e1dc06d87acc6a
jungwoogyun <jwg8910@naver.com>
2024-06-18 01:54:35 +0900

    .

--- src/main/java/com/creator/imageAndMusic/properties/SOCKET.java
+++ src/main/java/com/creator/imageAndMusic/properties/SOCKET.java
@@ -2,7 +2,7 @@ package com.creator.imageAndMusic.properties;
 
 public class SOCKET {
 
-    public static final String REQ_PATH ="wss://test.wooriac.store/ws/chat";
+    public static final String REQ_PATH ="wss://test.wooriac.store/wss/chat";
 
 //    public static final String REQ_PATH ="ws://172.30.1.d 870:8080/ws/chat";
 
