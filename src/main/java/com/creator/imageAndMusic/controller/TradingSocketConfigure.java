f52b0a649b9f3597f515eb3346783a1dc6f99dbf
jungwoogyun <jwg8910@naver.com>
2024-06-18 01:37:38 +0900

    .

--- src/main/java/com/creator/imageAndMusic/controller/TradingSocketConfigure.java
+++ src/main/java/com/creator/imageAndMusic/controller/TradingSocketConfigure.java
@@ -18,7 +18,9 @@ public class TradingSocketConfigure implements WebSocketConfigurer {
 그리고 setAllowedOrigins("*") 는 웹소켓 cors 정책으로 인해, 허용 도메인을 지정해줘야합니다.
 현재 저희는 테스트이기때문에 * 와일드카드로 모든 도메인을 열어줍시다.(실제로 개발하실때에는 보안상의 위험으로 와일드카드는 쓰시면안됍니다.)
  */
-        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+
+       // registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("https://test.wooriac.store");
 
     }
 }
