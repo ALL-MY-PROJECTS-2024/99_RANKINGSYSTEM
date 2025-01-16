e4288f9b6110ee2e007db59901cb11f355abc00d
junwoogyun <jwg8910@naver.com>
2025-01-16 11:35:44 +0900

    .

--- src/main/java/com/creator/imageAndMusic/properties/ADMINPROPERTIES.java
+++ src/main/java/com/creator/imageAndMusic/properties/ADMINPROPERTIES.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Component;
 public class ADMINPROPERTIES {
 
     public static String ADMIN_EMAIL="jwg135790@gmail.com";
-    public static String ADMIN_APP_KEY="nyoe oiuq rsca vcrt";
+    public static String ADMIN_APP_KEY="omms tfhk mhni hwpn";
 
     public static String IMPORT_UID="imp87380722";
+    //결제는 KG 이니시스 결제(V1)
 }
